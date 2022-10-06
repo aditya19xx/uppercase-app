81ad9db7d75296e878aaa1f2b8b2f624e4d612bd
Aditya Das <turjaadityadas@gmail.com>
2022-10-06 12:18:39 +0600

    dark mode switch added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import './App.css';
-import AboutUs from './components/About/AboutUs';
+// import AboutUs from './components/About/AboutUs';
 import Navbar from './components/Navbar/Navbar';
-// import TextForm from './components/Navbar/TextForm/TextForm';
+import TextForm from './components/Navbar/TextForm/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light'); //wheather dark mode is enabled or not
+
+  const togglemode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'black';
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-  <Navbar title="TexttUtils2" />
-  {/* <div className="container my-3">
+  <Navbar title="TexttUtils2" mode={mode} togglemode = {togglemode} />
+  <div className="container my-3">
     <TextForm heading="Enter Your text here" />
-  </div> */}
-  <AboutUs/>
+  </div>
+  {/* <AboutUs/> */}
     </>
   );
 }
