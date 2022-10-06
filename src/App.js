30683384a8b3b481b5ee82e717e29056e59606d0
Aditya Das <turjaadityadas@gmail.com>
2022-10-06 10:10:24 +0600

    dark mode enabled

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import AboutUs from './components/About/AboutUs';
 import Navbar from './components/Navbar/Navbar';
-import TextForm from './components/Navbar/TextForm/TextForm';
+// import TextForm from './components/Navbar/TextForm/TextForm';
 
 function App() {
   return (
     <>
   <Navbar title="TexttUtils2" />
-  <div className="container my-3">
+  {/* <div className="container my-3">
     <TextForm heading="Enter Your text here" />
-  </div>
+  </div> */}
+  <AboutUs/>
     </>
   );
 }
